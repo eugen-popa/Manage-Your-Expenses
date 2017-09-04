7b3fac1f3afc0583bf6293047622830fcd49ff12
Eugeniu POPA <eupopa@e1z2r9p1.42.us.org>
2017-09-03 22:16:41 -0700

    update

--- app.js
+++ app.js
@@ -1,25 +1,81 @@
 //Budget Controller
 var budgetControlerData = (function() {
-// same code
-  
+
+  var Expens = function(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+  };
+    
+    var Income = function(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+  };
+    
+    var data = {
+        
+        allItems:{
+            exp:[],
+            inc:[]
+        },
+        
+        total:{
+            exp: 0,
+            inc: 0
+        }
+    };
+    
+    
     
 })();
 
 // UI Controller
  var UIController = (function(){
-// same code
 
+     var DOMstr = {
+         inputType:'.add__type',
+         inputDescription: '.add__description',
+         inputValue: '.add__value',
+         inputBtn: '.add__btn'
+     };
+     
+     return{
+        getInput: function() {
+         return {
+            type: document.querySelector(DOMstr.inputType).value, // will be either inc or exp
+            description: document.querySelector(DOMstr.inputDescription).value,
+            value: document.querySelector(DOMstr.inputValue).value 
+         };
+        },
+        getDOMstr: function(){
+             return DOMstr;
+         }
+     };
  
  })();
 
 // Global APP Controller
 var controller = (function(budgetData, UICtrl) {
     
-    
-    var ctrlAddItem = function(){
-        // to do list
-        // 1. Get the filed nput data 
+    var setupEventListeners = function(){
         
+        var DOM = UICtrl.getDOMstr();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    }
+    
+    var ctrlAddItem = function() {
+        // to do list
+        // 1. Get the field input data 
+        var input = UICtrl.getInput();
+ 
         // 2. add the item to the budget controller
         
         // 3. add the item to the UI
@@ -27,19 +83,42 @@ var controller = (function(budgetData, UICtrl) {
         // 4. Calculeate the budget 
         
         // 5 . Display the buddget on the UI
-        
-        console.log('worcks')
-    }
+    };
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event){
-        if (event.keyCode === 13 || event.which === 13){
-           ctrlAddItem();
+    return {
+        init: function(){
+            console.log('start aplication.');
+            setupEventListeners();
         }
-    });
-    
+    };
     
 })(budgetControlerData, UIController);
+
+
+controller.init();
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
